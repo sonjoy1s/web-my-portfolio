3fde15e6f8e00f3653dc7837262273c3dabf889d
sonjoy chandra coach <adittysonjoy@gmail.com>
2025-05-03 15:53:34 +0600

    Add files via upload

---
+++ A_A_B_Again.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define joy_ma_soroshoti int main
+#define  fun solved void
+#define ll long long
+#define ci cin
+#define co cout
+using namespace std;
+void solved(){
+     int n; ci>>n;
+     int sum = (n%10) + (n/10);
+    co<<sum<<endl;
+}
+joy_ma_soroshoti(){
+     int tt; cin >> tt;
+    while (tt--){
+
+        solved();
+    }
+}
