928356a4d7007f604955fc0064351f34b0c1b291
sonjoy chandra coach <adittysonjoy@gmail.com>
2025-05-03 16:05:53 +0600

    Add files via upload

---
+++ A_Factorise_N_M.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define ci cin 
+#define co cout
+#define joy_ma_shoroshoti int main
+#define Hoo cout<<"YES";
+#define fun void solved
+#define w while
+using namespace std;
+fun(){
+    int n; cin>>n;
+    co<<n<<endl;
+}
+joy_ma_shoroshoti(){
+    int tt; cin>>tt; w(tt--){
+      solved();
+    }
+}
