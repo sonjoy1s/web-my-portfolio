e1990c6229df2ae1f752a51a68e245114247ef81
sonjoy chandra coach <adittysonjoy@gmail.com>
2025-05-03 15:59:28 +0600

    Add files via upload

---
+++ A_Catch_the_Coin.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define joy_ma_soroshoti int main
+#define  fun solved void
+#define ll long long
+#define ci cin
+using namespace std;
+void solved(){
+     ll a,b; cin>>a>>b;
+     if(b>=-1){
+        cout<<"YES"<<endl;
+     }
+     else{
+        cout<<"NO"<<endl;
+     }
+}
+joy_ma_soroshoti(){
+     int tt; cin >> tt;
+    while (tt--){
+
+        solved();
+    }
+}
