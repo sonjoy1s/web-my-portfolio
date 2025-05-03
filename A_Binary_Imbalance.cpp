23493331d0c56e4c6d4173273ec1c211671bf034
sonjoy chandra coach <adittysonjoy@gmail.com>
2025-05-03 15:57:18 +0600

    Add files via upload

---
+++ A_Binary_Imbalance.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define joy_ma_soroshoti int main
+#define  fun solved void
+#define ll long long
+#define ci cin
+using namespace std;
+joy_ma_soroshoti(){
+    int t; cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;  string s;
+        cin >> s;
+        if (s.find('0') == string::npos) {
+            cout << "NO" << endl;
+        } else {
+            cout << "YES" << endl;
+        }
+    }
+     
+}
