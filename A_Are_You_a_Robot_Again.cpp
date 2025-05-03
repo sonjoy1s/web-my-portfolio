abc65e91e900645b3c6aa840f9526d1a99605fca
sonjoy chandra coach <adittysonjoy@gmail.com>
2025-05-03 15:55:49 +0600

    Add files via upload

---
+++ A_Are_You_a_Robot_Again.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define ci cin 
+#define co cout
+#define sanjoy int main
+#define fun void solved
+#define w while
+using namespace std;
+
+sanjoy() {
+    co<<"safety"<<endl;
+    return 0;
+}
